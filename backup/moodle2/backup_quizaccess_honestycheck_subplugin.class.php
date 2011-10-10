74cfcda6fbc5bcd59d04b1b43cf481f6158fe209
Tim Hunt <T.J.Hunt@open.ac.uk>
2011-10-10 17:18:18 +0100

    Implement backup and restore hooks.

---
+++ backup/moodle2/backup_quizaccess_honestycheck_subplugin.class.php
@@ -0,0 +1,60 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Backup code for the quizaccess_honestycheck plugin.
+ *
+ * @package    quizaccess
+ * @subpackage honestycheck
+ * @copyright  2011 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+require_once($CFG->dirroot . '/mod/quiz/backup/moodle2/backup_mod_quiz_access_subplugin.class.php');
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * Provides the information to backup the honestycheck quiz access plugin.
+ *
+ * If this plugin is requires, a single
+ * <quizaccess_honestycheck><required>1</required></quizaccess_honestycheck> tag
+ * will be added to the XML in the appropriate place. Otherwise nothing will be
+ * added. This matches the DB structure.
+ */
+class backup_quizaccess_honestycheck_subplugin extends backup_mod_quiz_access_subplugin {
+
+    protected function define_quiz_subplugin_structure() {
+
+        // create XML elements
+        $subplugin = $this->get_subplugin_element();
+        $subplugin_wrapper = new backup_nested_element($this->get_recommended_name());
+        $subplugin_table_settings = new backup_nested_element('quizaccess_honestycheck',
+                null, array('honestycheckrequired'));
+
+        // connect XML elements into the tree
+        $subplugin->add_child($subplugin_wrapper);
+        $subplugin_wrapper->add_child($subplugin_table_settings);
+
+        // set source to populate the data
+        $subplugin_table_settings->set_source_table('quizaccess_honestycheck',
+                array('quizid' => backup::VAR_ACTIVITYID));
+
+        return $subplugin;
+    }
+}
---
+++ backup/moodle2/restore_quizaccess_honestycheck_subplugin.class.php
@@ -0,0 +1,65 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Restore code for the quizaccess_honestycheck plugin.
+ *
+ * @package    quizaccess
+ * @subpackage honestycheck
+ * @copyright  2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+require_once($CFG->dirroot . '/mod/quiz/backup/moodle2/restore_mod_quiz_access_subplugin.class.php');
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * Provides the information to restore the honestycheck quiz access plugin.
+ *
+ * If this plugin is required, a single
+ * <quizaccess_honestycheck><required>1</required></quizaccess_honestycheck> tag
+ * will be in the XML, and this needs to be written to the DB. Otherwise, nothing
+ * needs to be written to the DB.
+ */
+class restore_quizaccess_honestycheck_subplugin extends restore_mod_quiz_access_subplugin {
+
+    protected function define_quiz_subplugin_structure() {
+
+        $paths = array();
+
+        $elename = $this->get_namefor('');
+        $elepath = $this->get_pathfor('/quizaccess_honestycheck');
+        $paths[] = new restore_path_element($elename, $elepath);
+
+        return $paths;
+    }
+
+    /**
+     * Processes the quizaccess_honestycheck element, if it is in the file.
+     * @param array $data the data read from the XML file.
+     */
+    public function process_quizaccess_honestycheck($data) {
+        global $DB;
+
+        $data = (object)$data;
+        $data->quizid = $this->get_new_parentid('quiz');
+        $DB->insert_record('quizaccess_honestycheck', $data);
+    }
+}
